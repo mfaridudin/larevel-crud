b0dc8574f04b5c87139c5374b3acc8b1b2a48e1b
farid24452 <faridislamudin340@gmil.com>
2026-01-16 14:39:19 +0700

    Refactor delete buttons to use forms for proper HTTP DELETE requests in multiple views.

--- resources/views/hobbies/hobbies.blade.php
+++ resources/views/hobbies/hobbies.blade.php
@@ -75,28 +75,33 @@
                                                             </svg>
                                                         </template></iconify-icon>
                                                 </a>
-                                                <button class="action-btn" type="button">
-                                                    <iconify-icon icon="heroicons:trash"><template shadowrootmode="open">
-                                                            <style data-style="data-style">
-                                                                :host {
-                                                                    display: inline-block;
-                                                                    vertical-align: 0
-                                                                }
+                                                <form action="/hobbies/{{ $item->id }}/destroy" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button class="action-btn" type="submit">
+                                                        <iconify-icon icon="heroicons:trash"><template
+                                                                shadowrootmode="open">
+                                                                <style data-style="data-style">
+                                                                    :host {
+                                                                        display: inline-block;
+                                                                        vertical-align: 0
+                                                                    }
 
-                                                                span,
-                                                                svg {
-                                                                    display: block
-                                                                }
-                                                            </style><svg xmlns="http://www.w3.org/2000/svg" width="1em"
-                                                                height="1em" viewBox="0 0 24 24">
-                                                                <path fill="none" stroke="currentColor"
-                                                                    stroke-linecap="round" stroke-linejoin="round"
-                                                                    stroke-width="1.5"
-                                                                    d="m14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21q.512.078 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48 48 0 0 0-3.478-.397m-12 .562q.51-.088 1.022-.165m0 0a48 48 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a52 52 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a49 49 0 0 0-7.5 0">
-                                                                </path>
-                                                            </svg>
-                                                        </template></iconify-icon>
-                                                </button>
+                                                                    span,
+                                                                    svg {
+                                                                        display: block
+                                                                    }
+                                                                </style><svg xmlns="http://www.w3.org/2000/svg" width="1em"
+                                                                    height="1em" viewBox="0 0 24 24">
+                                                                    <path fill="none" stroke="currentColor"
+                                                                        stroke-linecap="round" stroke-linejoin="round"
+                                                                        stroke-width="1.5"
+                                                                        d="m14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21q.512.078 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48 48 0 0 0-3.478-.397m-12 .562q.51-.088 1.022-.165m0 0a48 48 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a52 52 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a49 49 0 0 0-7.5 0">
+                                                                    </path>
+                                                                </svg>
+                                                            </template></iconify-icon>
+                                                    </button>
+                                                </form>
                                             </div>
                                         </td>
                                     </tr>
@@ -119,52 +124,4 @@
         @csrf
         <button type="submit">logout</button>
     </form>
-
-    <button type="button" data-bs-toggle="modal" data-bs-target="#vertically_center"
-        class="btn inline-flex justify-center btn-outline-dark">Vertically Center</button>
-
-    <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-        id="disabled_animation" tabindex="-1" aria-labelledby="disabled_animation" aria-hidden="true">
-        <div class="modal-dialog top-0 !-translate-y-0 relative w-auto pointer-events-none">
-            <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding
-                    rounded-md outline-none text-current">
-                <div class="relative bg-white rounded-lg shadow dark:bg-slate-700">
-                    <!-- Modal header -->
-                    <div
-                        class="flex items-center justify-between p-5 border-b rounded-t dark:border-slate-600 bg-black-500">
-                        <h3 class="text-xl font-medium text-white dark:text-white capitalize">
-                            Vertically Center
-                        </h3>
-                        <button type="button" class="text-slate-400 bg-transparent hover:text-slate-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center
-                                dark:hover:bg-slate-600 dark:hover:text-white" data-bs-dismiss="modal">
-                            <svg aria-hidden="true" class="w-5 h-5" fill="#ffffff" viewbox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10
-                                        11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                            <span class="sr-only">Close modal</span>
-                        </button>
-                    </div>
-                    <!-- Modal body -->
-                    <div class="p-6 space-y-4">
-                        <h6 class="text-base text-slate-900 dark:text-white leading-6">
-                            Lorem ipsum dolor sit.
-                        </h6>
-                        <p class="text-base text-slate-600 dark:text-slate-400 leading-6">
-                            Oat cake ice cream candy chocolate cake apple pie. Brownie carrot cake candy canes. Cake
-                            sweet roll cake cheesecake cookie chocolate cake liquorice.
-                        </p>
-                    </div>
-                    <!-- Modal footer -->
-                    <div
-                        class="flex items-center justify-end p-6 space-x-2 border-t border-slate-200 rounded-b dark:border-slate-600">
-                        <button data-bs-dismiss="modal"
-                            class="btn inline-flex justify-center text-white bg-black-500">Accept</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
 </x-applayouts>
